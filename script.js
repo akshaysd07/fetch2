b151b77781d507179e8d84d109af0fb19384f132
akshaysd07 <80628284+akshaysd07@users.noreply.github.com>
2021-06-24 20:50:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const api= "https://thatcopy.pw/catapi/rest/";
+const api= "https://aws.random.cat/meow";
 fetch(api)
 .then(response=>response.json())
-.then(jsonData=>console.log(jsonData))
-catch{error=>console.log("Error in fetch:" +error)}
+//.then(jsonData=>console.log(jsonData))  ---choose from cosnole what to display
+
+.then(jsonData=>display(jsonData))
+.catch(error=>console.log("Error in fetch:" +error))
+
+var display=(data)=>{
+    document.getElementById('img').src=data.file;
+}
